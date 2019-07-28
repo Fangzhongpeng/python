e87ed7fecee46d527e6b645da3f129f6d45d26c1
fangzhongpeng <word2613182>
2019-07-28 14:06:55 +0800

    28

--- python/第三天/高阶函数.py
+++ python/第三天/高阶函数.py
@@ -1,22 +1,13 @@
-
 """
-
 高阶函数
-    a:把一个函数名当做实参传给另外一个函数（在不修改被装饰函数源代码的情况下为其添加功能）
-    b:返回值中包含函数名（不修改函数的调用方式）
-
-
-import time
+    1，把一个函数名当做实参传给另外一个函数
+    2, 返回值中包含函数名
 """
 def bar():
-    time.sleep(3)
-    print('in the bar')
-def test1(func):
-    print(func)     # 便于理解，新功能1
-    print("新加功能")   #新功能2
-    return func   # 返回值中包含函数名
+    print ("in the bar")
+def test1(func):     #将bar这个变量赋值给func这个变量
+#    print(func)      #打印门牌号的地址
+    func()           #门牌号+（）调用函数
+test1(bar)           #将bar这个实参赋值给func这个形参
 
-bar=test1(bar)   # 将新函数的返回值赋值给bar,新函数的返回值是一个func的地址（函数名），使得bar拥有新功能，
-
-#print(bar)
-bar()     # 结果是没修改bar函数调用方式的情况下为bar个函数添加了功能
+#总之一句话，函数名名的本质是指向一个内存地址，函数名+()就能运行
